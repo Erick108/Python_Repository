745deace71406f1ddd810b8ea5d174a7dc9394ae
Erick <erickvelasquez108@gmail.com>
2025-08-29 18:41:34 -0600

    Metodos matematicos

---
+++ mate.py
@@ -0,0 +1,19 @@
+def suma(a, b):
+    return a + b
+
+suma(1,2)
+
+def resta(a, b):
+    return a - b
+
+resta(2,2)
+
+def mult(a, b):
+    return a * b
+
+mult(2,2)
+
+def div(a, b):
+    return a/b
+
+div(2,2)
