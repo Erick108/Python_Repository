6ddd3de4f285a1988e6b90137189e3d666df8020
Erick <erickvelasquez108@gmail.com>
2025-11-26 13:08:27 -0600

    Comentado

---
+++ SEMANA17/semana17/appS17/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .forms import ProductoForm, CategoriaForm
+from django.http import HttpResponse
+from .models import Producto, Categoria
+
+def home(request):
+    return render(request,'home.html')
+
+def crear_producto(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Producto ingresado correctamente")
+    else:
+            form = ProductoForm()
+    return render (request, "crear_producto.html", {"form":form})
+
+def ver_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'verproductos.html', {"productos":productos})
+
+def crear_categorias(request):
+    if request.method == "POST":
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("vercategorias")
+    else:
+            form = CategoriaForm()
+    return render (request, "crear_categoria.html", {"form":form})
+    
+def ver_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'vercategorias.html', {"categorias":categorias})
